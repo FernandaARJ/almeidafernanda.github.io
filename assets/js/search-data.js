e4a5a5f60672d96da7f251a16524e0cc4e14df91
almeida-fernanda <35375508+almeida-fernanda@users.noreply.github.com>
2025-09-05 22:19:55 +0000

    Deploying to gh-pages from @ almeida-fernanda/almeida-fernanda.github.io@5cfb548bc85ffebb01678c1bb36ef3e06eedf820 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -448,35 +448,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%66%65%72%6E%61%6E%64%61.%61%6C%6D%65%69%64%61@%61%77%69.%75%6E%69-%68%65%69%64%65%6C%62%65%72%67.%64%65", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/fernanda-almeida-606912a2# your LinkedIn user name", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-work',
+        title: 'Work',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.awi.uni-heidelberg.de/en/professorships/international-development-economics/fernanda-almeida-msc", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/feralmeida96", "_blank");
         },
       },{
       id: 'light-theme',
